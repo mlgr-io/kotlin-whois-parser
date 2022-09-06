e9505c868250625132835f69981437e76ec72a92
Sascha Weyers <sw@mailguru.io>
2022-09-06 10:52:33 +0200

    fix unchecked cast

--- src/test/kotlin/io/mailguru/whois/TestUtil.kt
+++ src/test/kotlin/io/mailguru/whois/TestUtil.kt
@@ -8,13 +8,13 @@ object TestUtil {
     fun getResourceAsText(path: String): String? = object {}.javaClass.getResource(path)?.readText()
 }
 
-inline fun <reified T : Any, R> T.getPrivateProperty(name: String): R? = T::class
+inline fun <reified T : Any, reified R> T.getPrivateProperty(name: String): R? = T::class
     .memberProperties
     .firstOrNull { it.name == name }
     ?.apply { isAccessible = true }
     ?.get(this) as? R
 
-inline fun <reified T : Any, R> T.callPrivateMethod(name: String, vararg args: Any?): R? = T::class
+inline fun <reified T : Any, reified R> T.callPrivateMethod(name: String, vararg args: Any?): R? = T::class
     .functions
     .firstOrNull { it.name == name }
     ?.apply { isAccessible = true }
